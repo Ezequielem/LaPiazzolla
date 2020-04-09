382a46db1a2486defcd7b6fcf15d02fad1075a1f
Ezequielem <eemenseguet@hotmail.com>
2020-04-08 21:17:19 -0300

    Se realizo scalffolding de CURSO con las modificaciones necesarias

--- LaPiazzolla/Controllers/CursosController.cs
+++ LaPiazzolla/Controllers/CursosController.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using LaPiazzolla.Data;
 using LaPiazzolla.Models;
+using Microsoft.VisualBasic.CompilerServices;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace LaPiazzolla.Controllers
 {
@@ -22,6 +24,7 @@ namespace LaPiazzolla.Controllers
         // GET: Cursos
         public async Task<IActionResult> Index()
         {
+            var cursos = _context.Cursos.Include(c=>c.Profesor).AsNoTracking();
             return View(await _context.Cursos.ToListAsync());
         }
 
@@ -33,7 +36,7 @@ namespace LaPiazzolla.Controllers
                 return NotFound();
             }
 
-            var curso = await _context.Cursos
+            var curso = await _context.Cursos.Include(p=>p.Profesor).AsNoTracking()
                 .FirstOrDefaultAsync(m => m.CursoId == id);
             if (curso == null)
             {
@@ -43,18 +46,15 @@ namespace LaPiazzolla.Controllers
             return View(curso);
         }
 
-        // GET: Cursos/Create
         public IActionResult Create()
         {
+            listadoDeProfesores();
             return View();
         }
 
-        // POST: Cursos/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("CursoId,Nombre,PrecioMensual,Descripcion")] Curso curso)
+        public async Task<IActionResult> Create([Bind("CursoId,Nombre,PrecioMensual,Descripcion,ProfesorId")] Curso curso)
         {
             if (ModelState.IsValid)
             {
@@ -62,59 +62,133 @@ namespace LaPiazzolla.Controllers
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
+            listadoDeProfesores(curso.Profesor);
             return View(curso);
         }
 
-        // GET: Cursos/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null)
+            if (id==null)
             {
                 return NotFound();
             }
-
-            var curso = await _context.Cursos.FindAsync(id);
-            if (curso == null)
+            var curso = await _context.Cursos.AsNoTracking().FirstOrDefaultAsync(c=>c.CursoId==id);
+            if (curso==null)
             {
                 return NotFound();
             }
+            listadoDeProfesores(curso.Profesor);
             return View(curso);
         }
 
-        // POST: Cursos/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
+        [HttpPost, ActionName("Edit")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("CursoId,Nombre,PrecioMensual,Descripcion")] Curso curso)
+        public async Task<IActionResult> EditPost(int? id)
         {
-            if (id != curso.CursoId)
+            if (id==null)
             {
                 return NotFound();
             }
-
-            if (ModelState.IsValid)
+            var cursoAModdificar = await _context.Cursos.FirstOrDefaultAsync(c=>c.CursoId==id);
+            if (await TryUpdateModelAsync<Curso>(cursoAModdificar, "", c=>c.Nombre, c=>c.PrecioMensual, c=>c.Descripcion, c=>c.Profesor))
             {
                 try
                 {
-                    _context.Update(curso);
                     await _context.SaveChangesAsync();
                 }
-                catch (DbUpdateConcurrencyException)
+                catch (DbUpdateException /* ex */)
                 {
-                    if (!CursoExists(curso.CursoId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
+                    //Log the error (uncomment ex variable name and write a log.)
+                    ModelState.AddModelError("", "Unable to save changes. " +
+                "Try again, and if the problem persists, " +
+                "see your system administrator.");
                 }
                 return RedirectToAction(nameof(Index));
             }
-            return View(curso);
+            listadoDeProfesores(cursoAModdificar.Profesor);
+            return View(cursoAModdificar);
         }
+
+        public void listadoDeProfesores(object departamentoSeleccionado = null)
+        {
+            var consultaProfesores = from p in _context.Profesores
+                                     orderby p.Nombre
+                                     select p;
+            ViewBag.Profesor = new SelectList(consultaProfesores.AsNoTracking(), "ProfesorId", "Nombre", departamentoSeleccionado);
+        }
+
+        //// GET: Cursos/Create
+        //public IActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //// POST: Cursos/Create
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Create([Bind("CursoId,Nombre,PrecioMensual,Descripcion")] Curso curso)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _context.Add(curso);
+        //        await _context.SaveChangesAsync();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(curso);
+        //}
+
+        //// GET: Cursos/Edit/5
+        //public async Task<IActionResult> Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var curso = await _context.Cursos.FindAsync(id);
+        //    if (curso == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return View(curso);
+        //}
+
+        //// POST: Cursos/Edit/5
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Edit(int id, [Bind("CursoId,Nombre,PrecioMensual,Descripcion")] Curso curso)
+        //{
+        //    if (id != curso.CursoId)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            _context.Update(curso);
+        //            await _context.SaveChangesAsync();
+        //        }
+        //        catch (DbUpdateConcurrencyException)
+        //        {
+        //            if (!CursoExists(curso.CursoId))
+        //            {
+        //                return NotFound();
+        //            }
+        //            else
+        //            {
+        //                throw;
+        //            }
+        //        }
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(curso);
+        //}
 
         // GET: Cursos/Delete/5
         public async Task<IActionResult> Delete(int? id)
@@ -124,7 +198,7 @@ namespace LaPiazzolla.Controllers
                 return NotFound();
             }
 
-            var curso = await _context.Cursos
+            var curso = await _context.Cursos.Include(p=>p.Profesor).AsNoTracking()
                 .FirstOrDefaultAsync(m => m.CursoId == id);
             if (curso == null)
             {
